0f8f6efadcc0c4d47e840e0aa0773796b02182ae
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-03-31 22:32:39 +0200

    update app

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-app.use(logger(formatsLogger)); // до консолі виводяться повідомлення, який запрос куди пішов
+app.use(logger(formatsLogger)); 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", contactsRouter); // будь-який запит на /api/contacts буде оброблятись contactsRouter
+app.use("/api/contacts", contactsRouter); 
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });
