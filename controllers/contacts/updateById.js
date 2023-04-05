f95cbcce1c76fff94f2dc02395d4dc13310ad384
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-04-05 15:48:05 +0200

    delete console.log and update contacts.json

--- controllers/contacts/updateById.js
+++ controllers/contacts/updateById.js
@@ -1,13 +1,12 @@
 const { updateContact } = require("../../models/contacts");
 
 const updateById = async (req, res) => {
-  console.log(req.body)
   if (!req.body) {
     res.status(400).json({ message: "missing fields" });
   }
-  
-  const { contactId} = req.params;
-  console.log(req.params)
+
+  const { contactId } = req.params;
+
   const result = await updateContact(contactId, req.body);
   res.status(200).json(result);
 };
