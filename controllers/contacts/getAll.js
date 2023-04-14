2ae42cffae0b107b6d479075c96c5b0499cf503d
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-04-14 16:20:17 +0200

    add filter

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,12 +1,12 @@
 const Contact = require("../../models/contact");
 
 const getAll = async (req, res) => {
-  const { _id: owner } = req.user;
+  const { _id: owner, favorite = true } = req.user;
   // console.log(req.query) // в ньому містяться всі параметри пошуку
   const {page = 1, limit = 20} = req.query;
   const skip = (page-1) * limit;
 
-  const result = await Contact.find({ owner }, "", {skip, limit}).populate(
+  const result = await Contact.find({ owner, favorite }, "", {skip, limit}).populate(
     "owner",
     "email subscription"
   ); // візьме id яке зберегається в полі owner, пійди в колекцію(яка записана в ref), знайде там обєкт з таким id, і вставить його замість owner
