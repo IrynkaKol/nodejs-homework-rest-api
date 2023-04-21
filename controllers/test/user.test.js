41197d4f8641b6f0580349a83be86951f109f838
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-04-21 20:59:11 +0200

    add test

---
+++ controllers/test/user.test.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const request = require("supertest");
+const { users: ctrl } = require("../../controllers");
+
+const app = express();
+app.post("/users/register", ctrl.register);
+
+beforeAll(() => app.listen(3000));
+
+
+describe("test register controller", () => {
+  test("register return user", async () => {
+    const response = await request(app).post("/user/register");
+    console.log(response.status);
+  });
+});
