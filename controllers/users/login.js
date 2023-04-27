8958451ee9dca72aff0a3a007a3bfe144c025ec9
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-04-27 21:07:25 +0200

    update errors

--- controllers/users/login.js
+++ controllers/users/login.js
@@ -1,40 +1,42 @@
 const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const { User } = require("../../models/user");
-const { Unauthorized } = require("http-errors");
 
-const {SECRET_KEY} = process.env;
+const { HttpError } = require("../../helpers");
+
+// const { Unauthorized } = require("http-errors");
+
+const { SECRET_KEY } = process.env;
 
 const login = async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email }); // перевіряємо чи є користувач з такою поштою
   if (!user) {
-    throw new Unauthorized("Email or password is wrong");
+    throw new HttpError(401, "Email or password is wrong");
   }
 
-  if(!user.verify) {
-    throw new Unauthorized("Email not verified");
-
+  if (!user.verify) {
+    throw new HttpError(401, "Email not verified");
   }
 
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
-    throw new Unauthorized("Email or password is wrong");
+    throw new HttpError(401, "Email or password is wrong");
   }
 
   const payload = {
     id: user._id,
-  }
-  const token = jwt.sign(payload, SECRET_KEY, {expiresIn: "23h"});
-  await User.findByIdAndUpdate(user._id, {token}) // записуємо токинг до бази
+  };
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+  await User.findByIdAndUpdate(user._id, { token }); // записуємо токинг до бази
   res.json({
     token,
     user: {
-        email: user.email,
-        subscription: user.subscription,
-    }
-  })
+      email: user.email,
+      subscription: user.subscription,
+    },
+  });
 };
 
 module.exports = login;
