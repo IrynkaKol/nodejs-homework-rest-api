067c0dbb7deac36f9619dd829c8ff2ba053c87ca
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-04-27 22:28:12 +0200

    update

--- controllers/users/resendVerifyEmail.js
+++ controllers/users/resendVerifyEmail.js
@@ -12,8 +12,10 @@ const resendVerifyEmail = async (req, res) => {
   }
 
   if (user.verify) {
-    throw HttpError(400, "Verification email sent");
+    throw HttpError(400, "Verification has already been passed");
   }
+
+  
   const verifyEmail = {
     to: email,
     subject: "Verify email",
--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,17 +1,15 @@
-const sgMail = require('@sendgrid/mail');
+const sgMail = require("@sendgrid/mail");
 require("dotenv").config();
-const {SENDGRID_API_KEY} = process.env;
+const { SENDGRID_API_KEY } = process.env;
 
-sgMail.setApiKey(SENDGRID_API_KEY)
+sgMail.setApiKey(SENDGRID_API_KEY);
 
 const sendEmail = async (data) => {
+  const email = { ...data, from: "kolomoyka@gmail.com" };
 
-    const email = {...data, from: 'kolomoyka@gmail.com'}
-    await sgMail
-    .send(email);
-    return true;
-
-}
+  await sgMail.send(email);
+  return true;
+};
 module.exports = sendEmail;
 
 /*
