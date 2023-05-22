117e8c5f8e72e29cf133c52392b777f5797218a4
Iryna Kolomoiec <kolomoyka@gmail.com>
2023-05-22 22:28:40 +0200

    update

--- controllers/contacts/getById.js
+++ controllers/contacts/getById.js
@@ -2,17 +2,29 @@ const Contact = require("../../models/contact");
 const { NotFound } = require("http-errors");
 
 const getById = async (req, res) => {
-  const {_id: owner } = req.user;
-  const { contactId } = req.params;
-  console.log(req.params)
-  const result = await Contact.findOne({_id: contactId, owner});
+
+  
+    // const {_id: owner } = req.user;
+    const { contactId } = req.params;
+    const result = await Contact.findById(contactId); // const result = await Notice.findById({_id: noticeId, owner});
+  
+    if (!result) {
+      throw new NotFound("Not found");
+    }
+    res.json(result);
+    
+  
+  // const {_id: owner } = req.user;
+  // const { contactId } = req.params;
+  // console.log(req.params)
+  // const result = await Contact.findOne({_id: contactId, owner});
   
     
-  if (!result) {
-    throw new NotFound("Not found");
-  }
+  // if (!result) {
+  //   throw new NotFound("Not found");
+  // }
  
-  res.status(200).json(result);
+  // res.status(200).json(result);
   
 };
 module.exports = getById;
